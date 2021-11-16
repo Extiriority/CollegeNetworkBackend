1e88ad9df0e6a1dd091c31c6913798e99d6ede67
Giang <463189@student.fontys.nl>
2021-11-16 13:24:13 +0100

    Code formatting

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -10,61 +10,48 @@ namespace CollegeNetworkBackend1.Controllers
 {
     [Route("api")]
     [ApiController]
-    public class AuthController : Controller
-    {
+    public class AuthController : Controller {
         private readonly IUserRepository _repository;
         private readonly JwtService _jwtService;
 
-        public AuthController(IUserRepository repository, JwtService jwtService)
-        {
+        public AuthController(IUserRepository repository, JwtService jwtService) {
             _repository = repository;
             _jwtService = jwtService;
         }
         
         [HttpPost("register")]
-        public IActionResult register(RegisterDto dto)
-        {
-            var user = new User
-            {
+        public IActionResult register(RegisterDto dto) {
+            var user = new User {
                 firstName = dto.firstName,
                 lastName = dto.lastName,
                 email = dto.email,
                 password = BCrypt.Net.BCrypt.HashPassword(dto.password) 
             };
-
             return Created("success", _repository.create(user));
         }
 
         [HttpPost("login")]
-        public IActionResult login(LoginDto dto)
-        {
+        public IActionResult login(LoginDto dto) {
             var user = _repository.getByEmail(dto.email);
 
             if (user == null) return BadRequest(new { message = "Invalid credentials" });
             
-            if (!BCrypt.Net.BCrypt.Verify(dto.password, user.password))
-            {
+            if (!BCrypt.Net.BCrypt.Verify(dto.password, user.password)) {
                 return BadRequest(new { message = "Invalid credentials" });
             }
 
             var jwt = _jwtService.generate(user.id);
             
-            Response.Cookies.Append("jwt", jwt, new CookieOptions
-            {
+            Response.Cookies.Append("jwt", jwt, new CookieOptions {
                 HttpOnly = true
             });
             
-            return Ok(new
-            {
-                message = "success"
-            });
+            return Ok(new { user });
         }
 
         [HttpGet("user")]
-        public IActionResult user()
-        {
-            try
-            {
+        public IActionResult user() {
+            try {
                 var jwt = Request.Cookies["jwt"];
                 var token = _jwtService.verify(jwt);
                 int userId = int.Parse(token.Issuer);
@@ -72,19 +59,16 @@ namespace CollegeNetworkBackend1.Controllers
 
                 return Ok(user);
             }
-            catch (Exception _)
-            {
+            catch (Exception _) {
                 return Unauthorized();
             }
         }
 
         [HttpPost("logout")]
-        public IActionResult logout()
-        {
+        public IActionResult logout() {
             Response.Cookies.Delete("jwt");
 
-            return Ok(new
-            {
+            return Ok(new {
                 message = "success"
             });
         }
